047c4f2095df45d6f21c3cfb5e524519022bf412
Vikasverma9515 <142911666+Vikasverma9515@users.noreply.github.com>
2025-04-27 14:36:27 +0530

    firt commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:vikasverma9515@gmail.com">vikasverma951582@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/vikas-verma-264103275/">linkedin.com/Vikas-Verma</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Vikasverma9515">github.com/Vikasverma9515</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
